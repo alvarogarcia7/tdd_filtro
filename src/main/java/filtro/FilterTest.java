73eafb99e4c90b73f885a4530a6e1237dad10c0c
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-03-18 17:57:30 +0100

    refactor

--- src/main/java/filtro/FilterTest.java
+++ src/main/java/filtro/FilterTest.java
@@ -5,21 +5,26 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FilterTest {
+	
+	private List<String> result;
+
+	@Before
+	public void setUp() {
+		result = new ArrayList<String>();
+		result.add("FONTANERO");
+	}
 
 	@Test
 	public void input_with_single_word_is_converted_to_list_of_single_word() {
-		ArrayList<String> result = new ArrayList<String>();
-		result.add("FONTANERO");
 		assertEquals(result, filter("FONTANERO")); 
 	}
 	
 	@Test
 	public void input_with_lowcase_word_is_converted_to_list_of_uppercase_word(){
-		ArrayList<String> result = new ArrayList<String>();
-		result.add("FONTANERO");
 		assertEquals(result, filter("fontanero"));
 	}
 
