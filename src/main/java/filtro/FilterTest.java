4fecd7688d10bde9d80d34f076f312b82e2da713
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-03-19 10:48:22 +0100

    green: input_with_accentuated_word_is_converted_to_list_of_non_accentuated_word refactor: extract filter class

--- src/main/java/filtro/FilterTest.java
+++ src/main/java/filtro/FilterTest.java
@@ -2,6 +2,7 @@ package filtro;
 
 import static org.junit.Assert.assertEquals;
 
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -10,9 +11,36 @@ import java.util.regex.Pattern;
 import org.junit.Before;
 import org.junit.Test;
 
+import filtro.FilterTest.SearchQueryNormalizer;
+
 public class FilterTest {
 	
 	private List<String> result;
+	
+	public static class SearchQueryNormalizer {
+		
+
+		public SearchQueryNormalizer() {
+		}
+		
+		public List<String> filter(String searchQuery) {
+			List<String> result = new ArrayList<String>();
+
+			searchQuery = searchQuery.toUpperCase();
+			
+			searchQuery = Normalizer.normalize(searchQuery, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+			
+			String SUFFIX_PLURAL = "S";
+			if(searchQuery.endsWith(SUFFIX_PLURAL)){
+				searchQuery = searchQuery.substring(0, searchQuery.length()-1);
+			}
+			
+			result.add(searchQuery);
+			
+			return result;
+		}
+	}
+
 
 	@Before
 	public void setUp() {
@@ -32,7 +60,7 @@ public class FilterTest {
 	
 	@Test
 	public void input_with_accentuated_word_is_converted_to_list_of_non_accentuated_word(){
-		assertEquals(result, filter("FÓNTANERO"));
+		assertEquals(result, filter("FÓNTANÈRÓ"));
 	}
 	
 	@Test
@@ -41,24 +69,8 @@ public class FilterTest {
 	}
 
 	private List<String> filter(String searchQuery) {
-		List<String> result = new ArrayList<String>();
-
-		searchQuery = searchQuery.toUpperCase();
-		
-		searchQuery = searchQuery
-		.replace("Á","A")
-		.replace("É","E")
-		.replace("Í","I")
-		.replace("Ó","O")
-		.replace("Ú","U");
-		
-		if(searchQuery.endsWith("S")){
-			searchQuery = searchQuery.substring(0, searchQuery.length()-1);
-		}
-		
-		result.add(searchQuery);
-		
-		return result;
+		SearchQueryNormalizer normalizer = new SearchQueryNormalizer();
+		return normalizer.filter(searchQuery);
 	}
 
 }
